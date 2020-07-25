4f70b40190da384119c9eb50356ab8c3ea063cac
Nat Welch <nat@natwelch.com>
2020-07-25 19:56:50 +0000

    progress

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/icco/etu-cli
 
 go 1.14
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/machinebox/graphql v0.2.2
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/urfave/cli/v2 v2.2.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/machinebox/graphql"
 	"github.com/urfave/cli/v2"
 )
 
+type Config struct {
+	Env string
+	Key string
+}
+
+// Etu is the personifcation of time according to the Lakota.
 func main() {
+	cfg := &Config{}
 	app := &cli.App{
 		Name:  "etu",
 		Usage: "log a project to etu.natwelch.com",
@@ -16,40 +27,89 @@ func main() {
 				Name:    "print",
 				Aliases: []string{"p"},
 				Usage:   "print recent entries",
-				Action:  print,
+				Action:  cfg.Print,
 			},
 			{
 				Name:    "add",
 				Aliases: []string{"a"},
 				Usage:   "add a log",
-				Action:  add,
+				Action:  cfg.Add,
 			},
 		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:    "api_key",
-				Usage:   "authorize your user",
-				EnvVars: []string{"GQL_TOKEN"},
+				Name:        "api_key",
+				Usage:       "authorize your user",
+				EnvVars:     []string{"GQL_TOKEN"},
+				Destination: &cfg.Key,
 			},
 			&cli.StringFlag{
-				Name:    "env",
-				Usage:   "set which graphql server to talk to",
-				Value:   "production",
-				EnvVars: []string{"NAT_ENV"},
+				Name:        "env",
+				Usage:       "set which graphql server to talk to",
+				Value:       "production",
+				EnvVars:     []string{"NAT_ENV"},
+				Destination: &cfg.Env,
 			},
 		},
 	}
 
-	err := app.Run(os.Args)
+	err := app.RunContext(context.Background(), os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func add(c *cli.Context) error {
-	return nil
+type AddHeaderTransport struct {
+	T   http.RoundTripper
+	Key string
 }
 
-func print(c *cli.Context) error {
+func (adt *AddHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Add("X-API-AUTH", adt.Key)
+
+	return adt.T.RoundTrip(req)
+}
+
+func (cfg *Config) Client() (*graphql.Client, error) {
+	url := ""
+	switch cfg.Env {
+	case "production":
+		url = "https://graphql.natwelch.com/graphql"
+	case "development":
+		url = "http://localhost:9393/graphql"
+	default:
+		return nil, fmt.Errorf("unknown environment %q", cfg.Env)
+	}
+
+	httpclient := &http.Client{Transport: &AddHeaderTransport{T: http.DefaultTransport, Key: cfg.Key}}
+	return graphql.NewClient(url, graphql.WithHTTPClient(httpclient)), nil
+}
+
+func (cfg *Config) Add(c *cli.Context) error {
+	client, err := cfg.Client()
+	if err != nil {
+		return err
+	}
+
+	gql := `
+  mutation SaveLog($content: String!, $project: String!, $code: String!) {
+    insertLog(
+      input: { code: $code, description: $content, project: $project }
+    ) {
+      id
+      datetime
+    }
+  }
+`
+
+	req := graphql.NewRequest(gql)
+	req.Var("content", "test")
+	req.Var("code", "111")
+	req.Var("project", "test")
+
+	return client.Run(c.Context, req, nil)
+}
+
+func (cfg *Config) Print(c *cli.Context) error {
 	return nil
 }
